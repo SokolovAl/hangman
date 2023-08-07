06f1a77c2e9214fd309ca0968ddee38513ffa52b
Aleksandr Sokolov <sokolovmono@gmail.com>
2023-08-07 19:59:16 +0300

    add states for score, solution & letters

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import "./App.css";
 import Score from "./components/Score";
 import Solution from "./components/Solution";
 import Letters from "./components/Letters";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-        <Score/>
-        <Solution/>
-        <Letters/>
-    </div>
-  );
+    const generateLetterStatuses = () => {
+        let letterStatus = {};
+        for (let i = 65; i < 91; i++) {
+            letterStatus[String.fromCharCode(i)] = false;
+        }
+        return letterStatus;
+    };
+
+    const [letterStatus, setLetterStatus] = useState(generateLetterStatuses());
+    const [score, setScore] = useState(0);
+    const [solution, setSolution] = useState({
+        word: "CALM",
+        hint: "hint"
+    });
+
+    return (
+        <div className = "App">
+            <Score score = {score}/>
+            <Solution letterStatus = {letterStatus} solution = {solution}/>
+            <Letters letterStatus = {letterStatus}/>
+        </div>
+    );
 }
 
 export default App;
